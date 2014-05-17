d49273856ac1549bacc52b3ab93ee42b2aae1451
Jerzy Lekowski <jerzy@lekowski.pl>
2014-05-17 15:16:50 +0100

    Fix long calls settings

--- public/SOAP/server.php
+++ public/SOAP/server.php
@@ -17,7 +17,10 @@ use Battleships\Game\Data;
 use Battleships\Game\Manager;
 use Battleships\Misc;
 
+// don't cached WSDL file
 ini_set("soap.wsdl_cache_enabled", "0");
+// to use long calls
+ini_set('max_execution_time', CHECK_UPDATES_TIMEOUT);
 
 try {
     $oManager = new Manager(new Data(), new DB(DB_TYPE));
